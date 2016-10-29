c53533788b20c1d1967335c2d447d132795d3e9a
Andy Walpole <andywalpole@Andys-iMac.local>
2016-10-29 13:55:20 +0100

    added axios library

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,19 @@
+import axios from 'axios';
 const API_KEY = 'cc5273075ce8b12e4670051489bbc65b';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
+//q=London,gb&mode=xml& || q=London,gb&mode=json&
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function featchWeather(city = 'London') {
+
+	const url = `${ROOT_URL}&q=${city},gb`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+
+}
